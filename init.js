06b8539e07a7af6fac24d1924a9a065d357f1b73
golden98 <brayden.golden@example.com>
2023-12-04 03:15:42 -0500

    seems to be working

---
+++ init.js
@@ -0,0 +1,172 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'my.goldent3',
+    password: 'jyvcbbio',
+    database: 'my_goldent3_default'
+});
+
+connection.query(
+    "DROP TABLE IF EXISTS VIEWS_MOVIE;",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+connection.query(
+    "DROP TABLE IF EXISTS MOVIE;",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "DROP TABLE IF EXISTS PROFILE;",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+
+connection.query(
+    "DROP TABLE IF EXISTS USER_ACCOUNT;",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "CREATE TABLE USER_ACCOUNT (\
+        id SMALLINT(255) UNSIGNED NOT NULL AUTO_INCREMENT,\
+        Email VARCHAR(200) NOT NULL,\
+        FirstName VARCHAR(50),\
+        LastName VARCHAR(50),\
+        Phone VARCHAR(15),\
+        StreetAddress VARCHAR(100),\
+        City VARCHAR(50),\
+        State VARCHAR(20),\
+        Zip MEDIUMINT(255) UNSIGNED,\
+        Salt VARCHAR(20),\
+        Hash VARCHAR(64),\
+        UNIQUE KEY(Email),\
+        PRIMARY KEY(Id)\
+    )",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "CREATE TABLE PROFILE (\
+        id SMALLINT(255) UNSIGNED NOT NULL AUTO_INCREMENT,\
+        Name VARCHAR(50) NOT NULL,\
+        AcctID SMALLINT(255) UNSIGNED NOT NULL,\
+        PRIMARY KEY(Id),\
+        FOREIGN KEY(AcctID) REFERENCES USER_ACCOUNT(id) ON DELETE CASCADE\
+    );",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "CREATE TABLE MOVIE (\
+        id SMALLINT(255) UNSIGNED NOT NULL AUTO_INCREMENT,\
+        Name VARCHAR(50) NOT NULL,\
+        YearReleased SMALLINT(255) UNSIGNED,\
+        Runtime VARCHAR(10),\
+        Description VARCHAR(500),\
+        PRIMARY KEY(Id)\
+    );",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "CREATE TABLE VIEWS_MOVIE (\
+        MovieID SMALLINT(255) UNSIGNED NOT NULL,\
+        ProfileID SMALLINT(255) UNSIGNED NOT NULL,\
+        PercentComplete TINYINT(255) UNSIGNED,\
+        PRIMARY KEY (MovieID, ProfileID)\
+        FOREIGN KEY(ProfileID) REFERENCES PROFILE(id) ON DELETE CASCADE\
+        FOREIGN KEY(MovieID) REFERENCES MOVIE(id) ON DELETE CASCADE\
+    );",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO USER_ACCOUNT VALUES (1, 'email@gmail.com', 'Bob', 'Smith', '7777777', '1337 Wallstreet', 'New York City', 'New York', 33333, 'salt', 'hash');",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO USER_ACCOUNT VALUES (2, 'email2@gmail.com', 'Kimberly', 'Smith', '8888888', '1337 Wallstreet', 'New York City', 'New York', 33333, 'salt', 'hash');",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO PROFILE VALUES (1,'Bob', 1);",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO PROFILE VALUES (2,'Bob jr', 1);",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO PROFILE VALUES (3,'Kim', 2);",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO MOVIE VALUES (1,'300', 2006, '1hr57m','Description');",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO VIEWS_MOVIE VALUES (1, 1, 35);",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.query(
+    "INSERT INTO VIEWS_MOVIE VALUES (1, 3, 100);",
+    function(err, results, fields) {
+        console.log(results);
+        console.log(fields);
+    }
+);
+
+connection.end();
--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-// https://www.npmjs.com/package/ssh2
-
-const Client = require('ssh2').Client;
-
-const sshClient = new Client();
-
-sshClient.connect({
-    host: 'hopper.winthrop.edu',
-    port: 22,
-    username: 'goldent3',
-    privateKey: require('fs').readFileSync('C:/Users/brayd/.ssh/id_rsa')
-});
-
-sshClient.on('error', (err) => {
-    console.error('Error:', err);
-});
-
-
-
 const prompt = require('prompt-sync') ({sigint: true});
+const mysql = require('mysql2');
 
+var connection = connect();
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'my.goldent3',
+//     password: 'jyvcbbio',
+//     database: 'my_goldent3_default'
+// });
+
+// connection.execute('SELECT ? AS test;', ['FirstName'], (err, res, fields) => {
+//     console.log("it ran");
+//     console.log(err);
+//     console.log(res);
+//     console.log(fields);
+// });
+//ask();
+
+ask();
 
 function ask() {
-    let input = prompt('What would you like to do? (create, read, update, or delete)')
+    let input = prompt('What would you like to do? (create, read, update, delete, or done) ')
     let goodinput = false;
     while (!goodinput) {
         goodinput = true;
@@ -38,40 +38,215 @@ function ask() {
             case 'delete':
                 del();
                 break;
+            case 'done':
+                process.exit(0);
+                break;
             default:
                 goodinput = false;
-                input = prompt('Please enter create, read, update, or delete.');
+                input = prompt('Please enter create, read, update, delete, or done. ');
         }
     }
 }
 
 function create() {
-    let email = prompt('Enter an email:');
-    let firstName = prompt('Enter a firstName:');
-    let lastName = prompt('Enter a lastName:');
-    let phone = prompt('Enter a phone:');
-    let street = prompt('Enter a street:');
-    let city = prompt('Enter a city:');
-    let state = prompt('Enter a state:');
-    let zip = prompt('Enter a zip:');
-    let salt = prompt('Enter a salt:');
-    let hash = prompt('Enter a hash:');
+    let email = prompt('Enter an email: ');
+    let firstName = prompt('Enter a firstName: ');
+    let lastName = prompt('Enter a lastName: ');
+    let phone = prompt('Enter a phone: ');
+    let street = prompt('Enter a street address: ');
+    let city = prompt('Enter a city: ');
+    let state = prompt('Enter a state: ');
+    let zip = prompt('Enter a zip: ');
+    while(!Number.isInteger(Number(zip))) {
+        zip = prompt("zip must be an integer: ")
+    }
+    zip = Number(zip);
+    if (Number.isNaN(zip)) zip = 0;
+    let salt = prompt('Enter a salt: ');
+    let hash = prompt('Enter a hash: ');
+    
+    connection.ping();
+    connection.execute(
+        "INSERT INTO USER_ACCOUNT VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+        [email, firstName, lastName, phone, street, city, state, zip, salt, hash],
+        function(err, rows) {
+            console.log(err || "");
+            console.log(rows);
+            ask();
+        }
+    );
 };
 
 function read() {
-    let choice = prompt('Would you like to enter an email or id?');
+    let choice = prompt('Would you like to enter an email or id? ');
     while (choice != 'email' && choice != 'id') {
-        choice = prompt('Please either the word \'email\' or \'id\'');
+        choice = prompt('Please either the word \'email\' or \'id\' ');
+    }
+    if (choice == 'email') {
+        let email = prompt('Enter an email: ');
+        connection.ping();
+        connection.execute(
+            "select * from USER_ACCOUNT where email = ?",
+            [email],
+            function(err, rows) {
+                console.log(err || "");
+                console.log(rows);
+                ask();
+            }
+        );
+    } else {
+        let id = prompt('Enter an id: ');
+        connection.ping();
+        connection.execute(
+            "select * from USER_ACCOUNT where id = ?",
+            [id],
+            function(err, rows) {
+                console.log(err || "");
+                console.log(rows);
+                ask();
+            }
+        );
     }
 };
 
 function update() {
-    let choice = prompt('Would you like to enter an email or id?');
+    let choice = prompt('Would you like to identify the entry to be updated by an email or id? ');
     while (choice != 'email' && choice != 'id') {
-        choice = prompt('Please either the word \'email\' or \'id\'');
+        choice = prompt('Please either the word \'email\' or \'id\' ');
+    }
+    let ogemail, id;
+    if (choice == 'email') {
+        ogemail = prompt('Enter an email: ');
+    } else {
+        id = prompt('Enter an id: ');
+    }
+
+    console.log("For each column enter either the value to update to, or leave blank");
+    let email = prompt('Enter an email: ');
+    let firstName = prompt('Enter a firstName: ');
+    let lastName = prompt('Enter a lastName: ');
+    let phone = prompt('Enter a phone: ');
+    let street = prompt('Enter a street address: ');
+    let city = prompt('Enter a city: ');
+    let state = prompt('Enter a state: ');
+    let zip = prompt('Enter a zip: ');
+    while(!Number.isInteger(Number(zip)) && zip != "") {
+        zip = prompt("zip must be an integer: ");
+    }
+    if (zip != "") zip = Number(zip);
+    let salt = prompt('Enter a salt: ');
+    let hash = prompt('Enter a hash: ');
+
+    let vals = [];
+    let qstring = "";
+    if (email) {
+        qstring += "Email = ?,";
+        vals.push(email);
+    }
+    if (firstName) {
+        qstring += "FirstName = ?,";
+        vals.push(firstName);
+    }
+    if (lastName) {
+        qstring += "LastName = ?,";
+        vals.push(lastName);
+    }
+    if (phone) {
+        qstring += "Phone = ?,";
+        vals.push(phone);
+    }
+    if (street) {
+        qstring += "StreetAddress = ?,";
+        vals.push(street);
+    }
+    if (city) {
+        qstring += "City = ?,";
+        vals.push(city);
+    }
+    if (state) {
+        qstring += "State = ?,";
+        vals.push(state);
+    }
+    if (zip) {
+        qstring += "Zip = ?,";
+        vals.push(zip);
+    }
+    if (salt) {
+        qstring += "Salt = ?,";
+        vals.push(salt);
+    }
+    if (hash) {
+        qstring += "Hash = ?,";
+        vals.push(hash);
+    }
+
+    if (qstring.charAt(qstring.length-1) == ',') { // else nothing was updated
+        qstring = qstring.substring(0, qstring.length-1); // take off last comma
+        if (choice == 'email') {
+            qstring += " where Email = ?"
+            vals.push(ogemail);
+        } else {
+            qstring += " where id = ?"
+            id = Number(id);
+            vals.push(id);
+        }
+        console.log(qstring);
+        connection.ping();
+        connection.execute(
+            "update USER_ACCOUNT set " + qstring,
+            vals,
+            function(err, rows) {
+                console.log(err || "");
+                console.log(rows);
+                ask();
+            }
+        );
+
+
+    } else {
+        ask();
     }
 };
 
 function del() {
+    let choice = prompt('Would you like to enter an email or id? ');
+    while (choice != 'email' && choice != 'id') {
+        choice = prompt('Please either the word \'email\' or \'id\' ');
+    }
+    if (choice == 'email') {
+        let email = prompt('Enter an email: ');
+        connection.ping();
+        connection.execute(
+            "delete from USER_ACCOUNT where email = ?",
+            [email],
+            function(err, rows) {
+                console.log(err || "");
+                console.log(rows);
+                ask();
+            }
+        );
+    } else {
+        let id = prompt('Enter an id: ');
+        connection.ping();
+        connection.execute(
+            "delete from USER_ACCOUNT where id = ?",
+            [id],
+            function(err, rows) {
+                console.log(err || "");
+                console.log(rows);
+                ask();
+            }
+        );
+    }
+}
 
+
+function connect() {
+    console.log("connection established")
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'my.goldent3',
+        password: 'jyvcbbio',
+        database: 'my_goldent3_default'
+    });
 }
